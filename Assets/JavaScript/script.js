07f689ed10309c0a713034daf415ca22a3b39de2
GameW1zard <rivenloya@gmail.com>
2023-03-30 18:33:30 -0500

    update

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,11 +2,14 @@ var cityinput = $("#city")
 var searchbtn = $("#searchbutton")
 var li = $(".History-item")
 var ul = $("#history")
-var searchurl = "http://api.openweathermap.org/geo/1.0/direct?q=Austin,USA&limit=1&appid=f4033c6827b68c75bc4c8e651fc5dd75"
+const urlhold = "http://api.openweathermap.org/geo/1.0/direct?q="
+const geohold = "https://api.openweathermap.org/data/2.5/forecast?lat="
+var submiturl = ""
+var geosubmit = ""
+var lihold = ""
 
-
-
-
+function makeUrl () {submiturl = urlhold + cityinput.val() + ",USA&limit=5&appid=f4033c6827b68c75bc4c8e651fc5dd75"}
+function makeLocation () {geosubmit = geohold + latitude + "&lon=" + longitude + "&appid=f4033c6827b68c75bc4c8e651fc5dd75"}
 
 searchbtn.on("click", function () {
     
@@ -20,32 +23,62 @@ searchbtn.on("click", function () {
 
     ul.append(createli)
 
+    makeUrl()
+
+    getlocation()
+
 })
 
-function forcast () {
+function getlocation () {
 
-    fetch(searchurl)
+    fetch(submiturl)
 
     .then(response => response.json())
     
     .then (function (response) {
 
-        console.log(response)
+        console.log(response);
 
+        console.log(response[0].lat + " " + response[0].lon);
 
+        let latitude = response[0].lat
+        let longitude = response[0].lon
 
+        geosubmit = geohold + latitude + "&lon=" + longitude + "&cnt=40&appid=f4033c6827b68c75bc4c8e651fc5dd75&units=imperial"
+
+        forcast()
     })
     
-    ;
+    
 
 }
 
-//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={f4033c6827b68c75bc4c8e651fc5dd75}
+function forcast () {
+
+
+    fetch(geosubmit)
+
+    .then(response => response.json())
+    
+    .then (function (response) {
+        
+       console.log(response)
+
+
+    })
+
+
+
+
+
+}
+
+
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=f4033c6827b68c75bc4c8e651fc5dd75
 
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{US}&limit={5}&appid={f4033c6827b68c75bc4c8e651fc5dd75}
 
 
-ul.on("click", "li", function(event){console.log($(this).text())})
+ul.on("click", "li", function (event) {lihold = $(this).text(), submiturl = urlhold + lihold + ",USA&limit=5&appid=f4033c6827b68c75bc4c8e651fc5dd75", getlocation()})
 
 
-forcast()
