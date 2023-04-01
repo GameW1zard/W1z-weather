ba82632dec0e00c3ec2e6bed2eb427f02d384944
GameW1zard <rivenloya@gmail.com>
2023-04-01 01:14:39 -0500

    update
    moving systems

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,13 +2,18 @@ var cityinput = $("#city")
 var searchbtn = $("#searchbutton")
 var li = $(".History-item")
 var ul = $("#history")
+var todayul = $("#todayul")
 const urlhold = "http://api.openweathermap.org/geo/1.0/direct?q="
 const geohold = "https://api.openweathermap.org/data/2.5/forecast?lat="
+const dailyhold = "https://api.openweathermap.org/data/2.5/weather?lat="
+var todayurl = ""
 var submiturl = ""
 var geosubmit = ""
 var lihold = ""
-
-function makeUrl () {submiturl = urlhold + cityinput.val() + ",USA&limit=5&appid=f4033c6827b68c75bc4c8e651fc5dd75"}
+var latitude = ""
+var longitude = ""
+var iconcode = ""
+function makeUrl () {submiturl = urlhold + lihold + ",USA&limit=5&appid=f4033c6827b68c75bc4c8e651fc5dd75"}
 function makeLocation () {geosubmit = geohold + latitude + "&lon=" + longitude + "&appid=f4033c6827b68c75bc4c8e651fc5dd75"}
 
 searchbtn.on("click", function () {
@@ -23,11 +28,18 @@ searchbtn.on("click", function () {
 
     ul.append(createli)
 
+    lihold = cityinput.val()
+
     makeUrl()
 
     getlocation()
 
+    today()
+
 })
+
+
+
 
 function getlocation () {
 
@@ -46,10 +58,46 @@ function getlocation () {
 
         geosubmit = geohold + latitude + "&lon=" + longitude + "&cnt=40&appid=f4033c6827b68c75bc4c8e651fc5dd75&units=imperial"
 
+        todayurl = dailyhold + latitude + "&lon=" + longitude + "&appid=f4033c6827b68c75bc4c8e651fc5dd75&units=imperial"
+
         forcast()
+
+        today()
+
     })
     
     
+
+}
+
+function today () {
+
+    fetch(todayurl)
+
+    .then(response => response.json())
+    
+    .then (function (response) {
+        var iconurl =""
+        var iconcode = response.weather[0].icon
+        var imgtoday = $("#imgtoday")
+        let today = dayjs().format("DD/MM/YY")
+        let todaybody = $("#todaybody")
+        //today card
+        $(todaybody).children().eq(0).text(lihold + " " + today)
+        $(todayul).children().eq(0).text("Tempurture " + response.main.temp)
+        $(todayul).children().eq(1).text("Humidity " + response.main.humidity)
+        $(todayul).children().eq(2).text("Wind speed " + response.wind.speed)
+
+        console.log("icon " + iconcode)
+
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+        imgtoday.attr("src", iconurl)
+        //end today card
+        console.log(response)
+
+    })
+
 
 }
 
@@ -61,16 +109,15 @@ function forcast () {
     .then(response => response.json())
     
     .then (function (response) {
-        
-       console.log(response)
+        console.log(response)
+        let iconcode = response.list[9].weather[0].icon
+        let day = $("#day1image")
+        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        day.attr("src", iconurl)
+      
 
 
     })
-
-
-
-
-
 }
 
 
@@ -78,7 +125,12 @@ function forcast () {
 
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{US}&limit={5}&appid={f4033c6827b68c75bc4c8e651fc5dd75}
 
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=f4033c6827b68c75bc4c8e651fc5dd75
 
 ul.on("click", "li", function (event) {lihold = $(this).text(), submiturl = urlhold + lihold + ",USA&limit=5&appid=f4033c6827b68c75bc4c8e651fc5dd75", getlocation()})
+function now () {}
+
+//8 17 26 35
+
 
 
